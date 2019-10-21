2d6d2bc63c7b262eb99b8ff501292634230c7221
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-10-20 17:51:01 -0700

    Add retrieval of new image in s3

--- handler.py
+++ handler.py
@@ -1,4 +1,5 @@
 import boto3
+import cStringIO
 import os
 
 s3 = boto3.client('s3')
@@ -7,3 +8,22 @@ size = int(os.environ['THUMBNAIL_SIZE'])
 
 def s3_thumbnail_generator(event, context):
     print(event)
+
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['bucket']['key']
+
+    if not key.endswith("_thumbnail.png"):
+        image = get_s3_image(bucket, key)
+        thumbnail = image_to_thumbnail(image)
+
+
+def get_s3_image(bucket, key):
+    resp = s3.get_object(Bucket=bucket, Key=key)
+    image_content = resp['Body'].read()
+    file = cStringIO.StringIO(image_content)
+    
+
+
+def image_to_thumbnail(image):
+    return
+
