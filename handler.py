1d081ed9d88a22452afb1e6c751a57b1fe75f102
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-10-21 23:13:19 -0700

    Add uploading of new thumbnail to s3

--- handler.py
+++ handler.py
@@ -4,14 +4,15 @@ from PIL import Image, ImageOps
 import os
 
 s3 = boto3.client('s3')
+""" :type : pyboto3.s3 """
 size = int(os.environ['THUMBNAIL_SIZE'])
 
 
 def s3_thumbnail_generator(event, context):
-    print('event', event)
-
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
+    print('bucket', bucket)
+    print('key', key)
 
     print('is_thumbnail(key)', is_thumbnail(key))
 
@@ -22,14 +23,15 @@ def s3_thumbnail_generator(event, context):
         print('thumbnail', thumbnail)
         thumbnail_key = new_filename(key)
         print('thumbnail_key', thumbnail_key)
+        url = upload_to_s3(bucket, thumbnail_key, thumbnail)
+        return url
 
 
 def is_thumbnail(key):
     # TODO - can improve this so search term is last in filename
-    if key.find("_thumbnail.") == -1:
-        return False
-    else:
+    if key.find("_thumbnail.") > -1:
         return True
+    return False
 
 
 def get_s3_image(bucket, key):
@@ -46,9 +48,27 @@ def image_to_thumbnail(image):
 
 
 def new_filename(key):
-    split = key.split('.', 1)
+    split = key.rsplit('.', 1)
     base, extension = split[0], split[1]
-    return base + "_thumbnail." + extension
+    return f"{base}_thumbnail.{extension}"
 
 
+def upload_to_s3(bucket, key, image):
+    new_thumbnail = BytesIO()
+    file_extension = key.rsplit('.', 1)[1]
+    if file_extension == "jpg":
+        file_extension = "jpeg"  # For Pillow processing
+    image.save(new_thumbnail, file_extension)
+    new_thumbnail.seek(0)
 
+    response = s3.put_object(
+        ACL='public-read',
+        Body=new_thumbnail,
+        Bucket=bucket,
+        ContentType='image/'+file_extension,
+        Key=key
+    )
+    print('response', response)
+
+    url = f"{s3.meta.endpoint_url}/{bucket}/{key}"
+    print("url", url)
